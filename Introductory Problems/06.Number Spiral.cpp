53a30a3e33da1d3ab186da6038e9eb9af94c6067
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-08-17 12:38:46 +0600

    Create 06.Number Spiral.cpp

---
+++ Introductory Problems/06.Number Spiral.cpp
@@ -0,0 +1,82 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+
+// link : https://cses.fi/problemset/task/1071
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     even(x)         (x)%2
+#define     inn             int n;cin>>n;
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+    
+        ll r , c;
+        cin >> r >> c;
+        ll x = max(r , c);
+        ll index = x*(x-1);
+        index++;
+        //cout << index << endl;
+        if(x == r && x == c)
+            cout << index << endl;
+        else if(x == r)
+        {
+            if(x%2==1)
+                cout << index - (r-c)<< endl;
+            else
+                cout << index + (r-c)<< endl;
+        }
+        else if(x == c)
+        {
+            if(x%2==1)
+                cout << index + (c-r)<< endl;
+            else
+                cout << index - (c-r)<< endl;
+        }
+
+    }
+
+
+
+    return 0;
+}
